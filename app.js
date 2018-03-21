081857f7e412bc4b3a0b5b53e10e19c1007d4a29
Pranav Gupta <pranavgupta93@gmail.com>
2018-03-21 17:07:06 +0530

    included cover pic , textbox for user input .
    Also converted old JS code to ES6.

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-var fbToken='EAACEdEose0cBACcn7XuXjZBlTCkGSDUYkSZAkjywdmlxUxUHoZBjUOKQgXgzb91cZARF8ONBqBc1C3ZAr4p4ZBg9rUquqlTxzXC0M6vbkvjHCwd9aFm7MjomIGSmMIK3DTSsVvaBhSTqh6XO84rYE4SS2lErjL1yDSkqVgZACBRRWx8U2gFs4GweIds2Wkv7cJt6ykh1Qx5zwZDZD';
-$(document).ready(function(){
+//let fbToken='EAACEdEose0cBAAAseQ5pNGs9TPkTOmALlM4RpCby9F6691pDYMNSyK0XzOmY74BFJAIyxJQ9zNdbfAVzq6KZBSdureSMdSvKZAkZAEKCdhMWBiXOCFL4JcMGjlZBZCKmEkR2Wfjhld1cf5aqSe5Nst8gpqokm7NeDc9vSnDoW500oCyBBoZC0YiZBt7R8TxZCk079qG2uAno9QZDZD';
+$(document).ready(()=>{
 		$("#newsfeedbanner").hide();
+		$(".loader").hide();
 		$(".loader2").hide();
-		$.ajax(
+		$("#showfeedbutton").hide();
+		let fbToken=null;
+
+		
+		showprofile=()=>{
+			fbToken=$("#gettoken").val();
+			console.log(fbToken);
+			$.ajax(
 			{	type:"get",
-				url: 'https://graph.facebook.com/me?fields=picture.width(200).height(200),id,name,location,hometown,gender,work,relationship_status,email&access_token='+fbToken,
-				success:function(response){
-				//$("#profilepic").attr("src",  response.picture.data.url );     
+				url: 'https://graph.facebook.com/me?fields=picture.width(200).height(200),cover,id,name,location,hometown,gender,work,relationship_status,email&access_token='+fbToken,
+				success:(response)=>{
+				$("#gettokenblock").hide(200);
+				
+				$("#showfeedbutton").show();
+				//$("#profilepic").attr("src",  response.picture.data.url );  
+				$("#coverpic").html('<img src=' + response.cover.source + ' class="img-responsive" alt="not found" width="100%" height="100%">');   
 				$("#profilepic").html('<img src=' + response.picture.data.url + ' class="img-responsive" alt="not found">');  
 				$("#name").html('<h1>Name:</h1><h2>'+response.name+' </h2>');
 				$("#hometown").html('<h1>HomeTown:</h1><h2>'+response.hometown.name+' </h2>');
@@ -17,21 +29,23 @@ $(document).ready(function(){
 				$("#relationshipstatus").html('<h1>Relationship Status:</h1><h2>'+response.relationship_status+' </h2>');
 				$("#gender").html('<h1>Gender:</h1><h2>'+response.gender+' </h2>');
 			},//end of success
-			error:function(request,errorType,errorMessage)
+			error:(request,errorType,errorMessage)=>
 			{
+				$("#gettoken").val('');
 				alert("Please check if your token is valid and have required permissions then try again ");
 			},
 			timeout: 5000,
-			beforesend:function()
+			beforesend:()=>
 			{
 				$(".loader").show();
 			},
-			complete: function(){
+			complete: ()=>{
 			$('.loader').hide();
 		}
 
-		})//end of ajax call	
-		function showNewsFeed()
+		})//end of ajax call
+		}	
+		showNewsFeed=()=>
 		{
 			$("#showfeedbutton").hide();
 			$("#newsfeedbanner").show();
@@ -39,13 +53,24 @@ $(document).ready(function(){
 			$.ajax({
 				type:"get",
 				url:"https://graph.facebook.com/me?fields=feed{status_type,story,message,full_picture,link,type,name,source}&access_token="+fbToken,
-				success:function(response){
-					var i;
+				success:(response)=>{
+					let i;
 					for(i=0;i<=7;i++){
-						var feedseq=i+1;
+						let feedseq=i+1;
+						//console.log(response.feed.data[i]);
 					if(response.feed.data[i].status_type=='added_photos')
 					{
-						$("#feed"+i).html("<img src="+response.feed.data[i].full_picture+" ")
+						console.log(response.feed.data[i].full_picture);
+						$("#feed"+i).html("Feed #"+feedseq);
+							if(response.feed.data[i].story)
+							{
+								$("#feed"+i).append("<br>"+" "+response.feed.data[i].story);
+							}
+							if(response.feed.data[i].message)
+							{
+							$("#feed"+i).append("<br>"+" "+response.feed.data[i].message);
+							}
+						$("#feed"+i).append("<img src="+response.feed.data[i].full_picture+" class='img-responsive' width='100%'>")
 					}
 					else if(response.feed.data[i].status_type=='mobile_status_update')
 					{
@@ -82,6 +107,9 @@ $(document).ready(function(){
 							}
 							$("#feed"+i).append('<br><a href='+response.feed.data[i].link+' >'+'click here to get the link</a>');
 						}
+						else if (response.feed.data[i].type=='shared_story'){
+							$("#feed"+i).html("Feed #"+feedseq+"<br>"+response.feed.data[i].message+" <br>");
+						}
 						else{
 							//Declared this block to handle other exceptionl case then generic ones so that program doesn't throw error on run time
 						}
@@ -93,20 +121,21 @@ $(document).ready(function(){
 				}//end for loop
 
 				},	//end of success
-				error:function(request,errorType,errorMessage)
+				error:(request,errorType,errorMessage)=>
 				{	
 					alert("Please check if your token is valid and have required permissions then try again ");
 				},
 				timeout:5000,
-				beforesend:function()
+				beforesend:()=>
 				{
 					$(".loader2").show();
 				},
-				complete:function()
+				complete:()=>
 				{
 					$(".loader2").hide();
 				}
 			})//end of ajax call
 		}//end showNewsFeed
 		$('#showfeedbutton').on('click',showNewsFeed);
+		$('#tokensubmit').on('click',showprofile);
 	}) //end of document.ready
